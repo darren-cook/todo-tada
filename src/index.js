79f2cc8da4185af86b68332b16da4f2eb01b2728
darren-cook <darren.j.cook@uconn.edu>
2023-03-24 01:03:19 -0400

    nav toggle functionality and trnasitions progress

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const navButtons = document.querySelectorAll(".nav-button");
+const navToggle = document.querySelector("#nav-toggle");
 
 navButtons.forEach(navButton =>(
     navButton.addEventListener("click",function(){
@@ -6,3 +7,23 @@ navButtons.forEach(navButton =>(
         console.log(navButton.id)
     })
 ))
+
+navToggle.addEventListener("click",function(){
+    const sidePanel = document.querySelector("#side-panel");
+    const nav = document.querySelector("nav");
+    const navTexts = document.querySelectorAll(".nav-text");
+
+    sidePanel.classList.toggle("side-panel-open");
+    sidePanel.classList.toggle("side-panel-closed");
+
+    nav.classList.toggle("nav-open");
+    nav.classList.toggle("nav-closed");
+
+    navToggle.classList.toggle("nav-toggle-open");
+    navToggle.classList.toggle("nav-toggle-closed");
+
+    navTexts.forEach(navText => {
+        navText.classList.toggle("nav-text-open");
+        navText.classList.toggle("nav-text-closed");
+    })
+})
