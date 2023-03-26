30afd8e3a6bc0d2e1dd763b98c4fcb6d9339e4a5
darren-cook <darren.j.cook@uconn.edu>
2023-03-26 02:02:15 -0400

    calendar progress

--- src/calendar.js
+++ src/calendar.js
@@ -1,19 +1,34 @@
 import { eachDayOfInterval } from "date-fns";
 import { format } from "date-fns";
 
-const result = eachDayOfInterval({
-    start: new Date(2023, 2, 1),
-    end: new Date(2023, 3, 1)
-})
+let monthArray = getMonthString(new Date);
+let monthData = getMonthData(monthArray);
 
-function formatTest() {
-    const test = format(result[2], "EEEE")
-    console.log("test")
-    console.log(test)
+function getMonthString(date){
+    const year = format(date, "yyyy")-0;
+    const month = format(date, "L") -1;
+    const day = 1;
+    return([year, month, day]);
 }
 
-formatTest()
+function getMonthData(monthArray){
+    const monthDataFormatted = [];
+    const monthData = eachDayOfInterval({
+        start: new Date(monthArray[0], monthArray[1], monthArray[2]),
+        end: new Date(monthArray[0], monthArray[1]+1, monthArray[2])
+    })
+    monthData.pop();
+    monthData.forEach(date => {
+        const year = format(date, "yyyy");
+        const month = format(date, "LLL");
+        const day = format(date, "d");
+        monthDataFormatted.push([year, month, day]);
+    });
+    return(monthData)
+}
 
-// console.log(result)
+console.log(monthData);
 
-export { result }
+monthArray[1] = monthArray[1]-1;
+
+console.log(getMonthData(monthArray))
