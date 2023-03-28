e8a91b3283d3d6ee283de1ee0184c0eb584e8f69
darren-cook <darren.j.cook@uconn.edu>
2023-03-28 01:03:11 -0400

    reset design and made progress on new layout

--- src/index.js
+++ src/index.js
@@ -1,34 +1,10 @@
-import { result } from "./calendar";
-
-const navButtons = document.querySelectorAll(".nav-button");
-const navToggle = document.querySelector("#nav-toggle");
+const navButtons = document.querySelectorAll(".nav-item");
 
 navButtons.forEach(navButton =>(
     navButton.addEventListener("click",function(){
-        event.stopPropagation();
         const navActive = document.querySelector(".nav-active");
 
         navActive.classList.toggle("nav-active");
         navButton.classList.toggle("nav-active");
     })
 ))
-
-navToggle.addEventListener("click",function(){
-    const sidePanel = document.querySelector("#side-panel");
-    const nav = document.querySelector("nav");
-    const navTexts = document.querySelectorAll(".nav-text");
-
-    sidePanel.classList.toggle("side-panel-open");
-    sidePanel.classList.toggle("side-panel-closed");
-
-    nav.classList.toggle("nav-open");
-    nav.classList.toggle("nav-closed");
-
-    navToggle.classList.toggle("nav-toggle-open");
-    navToggle.classList.toggle("nav-toggle-closed");
-
-    navTexts.forEach(navText => {
-        navText.classList.toggle("nav-text-open");
-        navText.classList.toggle("nav-text-closed");
-    })
-})
