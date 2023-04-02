efc7f736174bdee23e1990de51164aa64cf2c790
darren-cook <darren.j.cook@uconn.edu>
2023-04-02 01:35:09 -0400

    calendar functions progress

--- src/calendar.js
+++ src/calendar.js
@@ -2,10 +2,10 @@ import { eachDayOfInterval } from "date-fns";
 import { format } from "date-fns";
 import { Chart } from 'chart.js/auto';
 
-let monthArray = getMonthString(new Date);
+let monthArray = getMonthArray(new Date);
 let monthData = getMonthData(monthArray);
 
-function getMonthString(date){
+function getMonthArray(date){
     const year = format(date, "yyyy")-0;
     const month = format(date, "L")-1;
     const day = 1;
@@ -13,54 +13,95 @@ function getMonthString(date){
 }
 
 function getMonthData(monthArray){
-    const monthDataFormatted = [];
-    const monthData = eachDayOfInterval({
+    const monthData = [];
+    const monthRange = eachDayOfInterval({
         start: new Date(monthArray[0], monthArray[1], monthArray[2]),
         end: new Date(monthArray[0], monthArray[1]+1, monthArray[2])
     })
-    monthData.pop();
-    monthData.forEach(date => {
+    monthRange.pop();
+    monthRange.forEach(date => {
         const year = format(date, "yyyy");
         const month = format(date, "LLL");
         const day = format(date, "d");
-        const dayOf = format(date, "iii");
-        monthDataFormatted.push([year, month, day, dayOf]);
+        const dayOfWeek = format(date, "iii");
+        const numOfWeek = format(date, "e");
+        monthData.push([year, month, day, dayOfWeek, numOfWeek]);
     });
-    return(monthDataFormatted)
+    return(monthData)
 }
 
 function calendarTest(){
-    console.log("test")
-
-    console.log(monthArray)
-    console.log(monthData)
-    
+    generateTable(monthData);
     const ctx = document.getElementById('myChart');
     
     new Chart(ctx, {
-    type: 'doughnut',
-    data: {
-        labels: ['Yellow', 'Green', 'Orange', 'Pink'],
-        datasets: [{
-            data: [7, 5, 3, 5],
-            backgroundColor: ["yellow", "green", "orange", "pink"],
-            borderWidth: .5,
-            cutout: '70%',
-        }]
-    },
-    options: {
-        responsive: true,
-        maintainAspectRatio: false,
-        plugins: {
-            tooltip: {
-                enabled: false,
-            },
-            legend: {
-                display: false,
-            },
+        type: 'doughnut',
+        data: {
+            labels: ['Complete', 'Overdue', 'High Priority', 'Regular Priority', 'Recurring'],
+            datasets: [{
+                data: [7, 5, 3, 5, 2],
+                backgroundColor: ["green", "red", "orange", "yellow", "cyan"],
+                borderWidth: .5,
+                cutout: '70%',
+            }]
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            plugins: {
+                tooltip: {
+                    enabled: false,
+                },
+                legend: {
+                    display: false,
+                },
+            }
         }
+        });
     }
-    });
+
+function generateTable(monthData) {
+    const table = document.getElementById("calendar-table");
+    let count = 0;
+
+    for(let week=0; week<6; week++){
+        const row = document.createElement("tr");
+
+        for(let day=0; day<7; day++){
+            const cell = document.createElement("td");
+            cell.id = `cell${count}`;
+
+            const date = document.createElement("p");
+            date.classList.add("chart-date");
+            cell.appendChild(date);
+
+            const chartContainer = document.createElement("div");
+            chartContainer.classList.add("chart-container");
+            cell.appendChild(chartContainer);
+
+            const total = document.createElement("p");
+            total.classList.add("chart-total");
+            cell.appendChild(total);
+
+            row.appendChild(cell)
+
+            count++;
+        }
+
+        table.appendChild(row);
+    }
+
+    fillTable(monthData);
+}
+
+function fillTable(monthData) {
+    const chartDates = document.querySelectorAll(".chart-date");
+    const firstNumOfWeek = (monthData[0][4])-1;
+
+    for(let i=firstNumOfWeek, j=0; j<monthData.length; i++, j++){
+        chartDates[i].innerHTML = monthData[j][2];
+    }
+    console.log(monthData)
 }
 
 export { calendarTest }
